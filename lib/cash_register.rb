60d34f4a587094b7c89c8af9108383cbb7298b43
Monica Tamaru Jones <mntamaru@gmail.com>
2018-02-17 17:52:33 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -11,7 +11,11 @@ class CashRegister
   
   def add_item (title, price, quantity = 1)
     @total += price * quantity
-    @@items << title * quantity
+    while quantity > 0
+      @@items << title
+      quantity -= 1
+    end
+    
   end
   
   def apply_discount
